c8069cc235e233a99d637941875e3c367088531d
Alexei Vinchester <alexei.vinnichek@mail.ru>
2024-03-28 17:58:35 +0300

    create data of pupils

---
+++ src/tutoringData/Pupil.js
@@ -0,0 +1,12 @@
+export class Pupil {
+    constructor(id, name, price, form){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.form = form;
+    }
+
+    sayPrice(){
+        return this.price;
+    }
+}
---
+++ src/tutoringData/pupils.js
@@ -0,0 +1,13 @@
+import { Pupil } from "./Pupil";
+
+export const pupils = [
+    new Pupil(1, 'Anton and Ilya', 30, 6),
+    new Pupil(2, 'Diana', 25, 5),
+    new Pupil(3, 'Bogdana', 25, 5),
+    new Pupil(4, 'Saveliy', 25, 6),
+    new Pupil(5, 'Tihon', 40, 4),
+    new Pupil(6, 'Darya', 25, 6),
+    new Pupil(7, 'Kirill green', 25, 6),
+    new Pupil(8, 'Kirill', 25, 6),
+];
+
